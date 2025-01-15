cabe02e6c35244fde9e0d4fc4177a7a2812d2a4f
Вадим Лилеков <vokelilekov@gmail.com>
2025-01-15 23:17:53 +0300

    'Ivy_update'

--- IvyGameWPF/IvyGameLibrary/Class1.cs
+++ IvyGameWPF/IvyGameLibrary/Class1.cs
@@ -14,148 +14,198 @@ namespace MahjongGameLibrary
         public bool IsAvailable { get; set; } = false;
     }
 
+    public enum Difficulty
+    {
+        Easy = 5,
+        Medium = 10,
+        Hard = 15
+    }
+
+    public enum Shape
+    {
+        Pyramid,
+        Heart
+    }
+
     public class GameField
     {
         public List<Tile> Tiles { get; private set; }
-        private const int MaxLayers = 10;
         private const int TileTypes = 10;
+        public int MaxLayers;
+        private Shape selectedShape;
 
-        public GameField()
+        public GameField(Difficulty difficulty, Shape shape, bool ensureSolvable = true)
         {
+            MaxLayers = (int)difficulty;
+            selectedShape = shape;
             Tiles = new List<Tile>();
-            GenerateTiles();
+
+            if (ensureSolvable)
+            {
+                GenerateSolvableFigure();
+            }
+            else
+            {
+                GenerateRandomFigure();
+            }
         }
 
-        private void GenerateTiles()
+        private void GenerateSolvableFigure()
+        {
+            //do
+            //{
+                Tiles.Clear();
+                GenerateFigureWithLogic();
+                ShuffleTiles();
+                UpdateTileAvailability();
+            //}
+            //while (!IsSolvable());
+        }
+
+        private void GenerateRandomFigure()
         {
             Tiles.Clear();
             var random = new Random();
-
-            int figureType = random.Next(0, 4);
-            int[] typeCounts = new int[TileTypes];
-
-            switch (figureType)
-            {
-                case 0: 
-                    GeneratePyramid(typeCounts);
-                    break;
-                case 1:
-                    GenerateStairs(typeCounts);
-                    break;
-                case 2:
-                    GenerateChessPattern(typeCounts);
-                    break;
-                case 3:
-                    GenerateDiamond(typeCounts);
-                    break;
-            }
-
+            var typeCounts = new int[TileTypes];
+            EnsureTripleTiles(typeCounts);
+            GenerateSymmetricalFigures(typeCounts);
             ShuffleTiles();
             UpdateTileAvailability();
         }
+
+        private void GenerateFigureWithLogic()
+        {
+            var random = new Random();
+            var typeCounts = new int[TileTypes];
+            EnsureTripleTiles(typeCounts);
+            GenerateSymmetricalFigures(typeCounts);
+        }
+
+        private void EnsureTripleTiles(int[] typeCounts)
+        {
+            int totalTiles = MaxLayers * MaxLayers * 3;
+            int repeatsPerType = Math.Max(3, totalTiles / TileTypes);
+
+            for (int i = 0; i < TileTypes; i++)
+            {
+                typeCounts[i] = repeatsPerType;
+            }
+
+            int remainingTiles = totalTiles - TileTypes * repeatsPerType;
+            var random = new Random();
+
+            while (remainingTiles > 0)
+            {
+                int randomType = random.Next(TileTypes);
+                typeCounts[randomType]++;
+                remainingTiles--;
+            }
+        }
+
+        private void GenerateSymmetricalFigures(int[] typeCounts)
+        {
+            if (selectedShape == Shape.Pyramid)
+            {
+                GeneratePyramid(typeCounts);
+            }
+            else if (selectedShape == Shape.Heart)
+            {
+                GenerateHeart(typeCounts);
+            }
+        }
+
         private void GeneratePyramid(int[] typeCounts)
         {
-            int layers = 5;
-            int tileIndex = 0;
+            var random = new Random();
+            var buffer = CreateTileBuffer(typeCounts);
 
-            for (int layer = 0; layer < layers; layer++)
+            int centerOffset = MaxLayers / 2;
+            int bufferIndex = 0;
+
+            for (int layer = 0; layer < MaxLayers; layer++)
             {
-                for (int x = 0; x < layers - layer; x++)
+                int layerSize = layer + 1;
+
+                for (int x = 0; x < layerSize; x++)
                 {
-                    for (int y = 0; y < layers - layer; y++)
+                    for (int y = 0; y < layerSize; y++)
                     {
-                        int type = GetNextTileType(typeCounts);
-                        Tiles.Add(new Tile
-                        {
-                            Index = tileIndex++,
-                            Type = type,
-                            Layer = layer,
-                            X = x + layer,
-                            Y = y + layer
-                        });
+                        if (bufferIndex >= buffer.Count) break;
+
+                        var tile = buffer[bufferIndex++];
+                        tile.Layer = layer;
+                        tile.X = centerOffset - layer / 2 + x;
+                        tile.Y = centerOffset - layer / 2 + y;
+
+                        Tiles.Add(tile);
                     }
                 }
             }
         }
 
-        private void GenerateStairs(int[] typeCounts)
+        private void GenerateHeart(int[] typeCounts)
         {
-            int steps = 6;
-            int tileIndex = 0;
+            var random = new Random();
+            var buffer = CreateTileBuffer(typeCounts);
+            int[,] heartMatrix = GetHeartMatrix();
 
-            for (int step = 0; step < steps; step++)
+            int rows = heartMatrix.GetLength(0);
+            int cols = heartMatrix.GetLength(1);
+            int bufferIndex = 0;
+
+            for (int layer = 0; layer < rows; layer++)
             {
-                for (int x = 0; x <= step; x++)
+                for (int x = 0; x < cols; x++)
                 {
-                    int type = GetNextTileType(typeCounts);
-                    Tiles.Add(new Tile
+                    if (heartMatrix[layer, x] == 1 && bufferIndex < buffer.Count)
+                    {
+                        var tile = buffer[bufferIndex++];
+                        tile.Layer = layer;
+                        tile.X = x - cols / 4;
+                        tile.Y = layer;
+
+                        Tiles.Add(tile);
+                    }
+                }
+            }
+        }
+
+        private List<Tile> CreateTileBuffer(int[] typeCounts)
+        {
+            int tileIndex = 0;
+            var buffer = new List<Tile>();
+
+            for (int type = 0; type < TileTypes; type++)
+            {
+                for (int count = 0; count < typeCounts[type]; count++)
+                {
+                    buffer.Add(new Tile
                     {
                         Index = tileIndex++,
                         Type = type,
-                        Layer = step,
-                        X = x,
-                        Y = step - x
+                        IsAvailable = true
                     });
                 }
             }
+
+            var random = new Random();
+            return buffer.OrderBy(_ => random.Next()).ToList();
         }
 
-        private void GenerateChessPattern(int[] typeCounts)
+        private int[,] GetHeartMatrix()
         {
-            int size = 8;
-            int tileIndex = 0;
+            int size = MaxLayers;
+            var heart = new int[size, size * 2 - 1];
 
-            for (int x = 0; x < size; x++)
+            for (int i = 0; i < size; i++)
             {
-                for (int y = 0; y < size; y++)
+                for (int j = size - i - 1; j < size + i; j++)
                 {
-                    if ((x + y) % 2 == 0)
-                    {
-                        int type = GetNextTileType(typeCounts);
-                        Tiles.Add(new Tile
-                        {
-                            Index = tileIndex++,
-                            Type = type,
-                            Layer = 0,
-                            X = x,
-                            Y = y
-                        });
-                    }
+                    heart[i, j] = 1;
                 }
             }
-        }
 
-        private void GenerateDiamond(int[] typeCounts)
-        {
-            int radius = 4;
-            int tileIndex = 0;
-
-            for (int x = -radius; x <= radius; x++)
-            {
-                for (int y = -radius; y <= radius; y++)
-                {
-                    if (Math.Abs(x) + Math.Abs(y) <= radius)
-                    {
-                        int type = GetNextTileType(typeCounts);
-                        Tiles.Add(new Tile
-                        {
-                            Index = tileIndex++,
-                            Type = type,
-                            Layer = 0,
-                            X = x + radius,
-                            Y = y + radius
-                        });
-                    }
-                }
-            }
-        }
-
-        private int GetNextTileType(int[] typeCounts)
-        {
-            int minCount = typeCounts.Min();
-            int type = Array.IndexOf(typeCounts, minCount);
-            typeCounts[type]++;
-            return type;
+            return heart;
         }
 
         private void ShuffleTiles()
@@ -164,8 +214,46 @@ namespace MahjongGameLibrary
             Tiles = Tiles.OrderBy(_ => random.Next()).ToList();
         }
 
+        //private bool IsSolvable()
+        //{
+        //    var simulatedTiles = Tiles.ToList();
+        //    var collectionField = new CollectionField();
 
-        public void UpdateTileAvailability()
+        //    while (simulatedTiles.Count > 0)
+        //    {
+        //        var availableTiles = simulatedTiles.Where(t => t.IsAvailable).ToList();
+
+        //        if (!availableTiles.Any())
+        //        {
+        //            return false;
+        //        }
+
+        //        foreach (var tile in availableTiles.Take(7))
+        //        {
+        //            collectionField.AddTile(tile);
+        //            simulatedTiles.Remove(tile);
+        //        }
+
+        //        var triplets = collectionField.CheckForTriplets();
+        //        if (!triplets.Any())
+        //        {
+        //            return false;
+        //        }
+
+        //        UpdateTileAvailability(simulatedTiles);
+        //    }
+
+        //    return true;
+        //}
+
+
+        public void RemoveTile(Tile tile)
+        {
+            Tiles.Remove(tile);
+            UpdateTileAvailability();
+        }
+
+        private void UpdateTileAvailability()
         {
             foreach (var tile in Tiles)
             {
@@ -175,24 +263,12 @@ namespace MahjongGameLibrary
                     Math.Abs(t.Y - tile.Y) <= 1);
             }
         }
-
-        public void RemoveTile(Tile tile)
-        {
-            Tiles.Remove(tile);
-            UpdateTileAvailability();
-        }
-
-        public bool IsGameWon()
-        {
-            return !Tiles.Any();
-        }
     }
 
     public class CollectionField
     {
         private const int MaxFieldSize = 7;
         public List<Tile> Tiles { get; private set; }
-
         public bool IsFull => Tiles.Count >= MaxFieldSize;
 
         public CollectionField()
--- IvyGameWPF/IvyGameWPF/MainWindow.xaml.cs
+++ IvyGameWPF/IvyGameWPF/MainWindow.xaml.cs
@@ -16,11 +16,19 @@ namespace MahjongGame
         private CollectionField collectionField;
         private BitmapImage[] tileSprites;
         private const string SpriteFolder = "Resources";
+        private Difficulty selectedDifficulty = Difficulty.Easy;
+        private Shape selectedShape;
+        private int tileSize = 70;
 
         public MainWindow()
         {
             InitializeComponent();
             LoadSprites();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
             StartNewGame();
         }
 
@@ -28,40 +36,56 @@ namespace MahjongGame
         {
             tileSprites = Directory.GetFiles(SpriteFolder, "*.png")
                                     .OrderBy(f => f)
-                                    .Select(f => new BitmapImage(new Uri(System.IO.Path.GetFullPath(f))))
+                                    .Select(f => new BitmapImage(new Uri(Path.GetFullPath(f))))
                                     .ToArray();
         }
 
         private void StartNewGame()
         {
-            gameField = new GameField();
+            Array shapes = Enum.GetValues(typeof(Shape));
+            Random random = new Random();
+            selectedShape = (Shape)shapes.GetValue(random.Next(shapes.Length));
+            gameField = new GameField(selectedDifficulty, selectedShape, ensureSolvable: true);
             collectionField = new CollectionField();
             RenderGameField();
             RenderCollectionField();
         }
 
+        private void DifficultySelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DifficultySelector.SelectedItem is ComboBoxItem selectedItem)
+            {
+                selectedDifficulty = Enum.TryParse(selectedItem.Tag.ToString(), out Difficulty difficulty) ? difficulty : Difficulty.Easy;
+                StartNewGame();
+            }
+        }
+
         private void RenderGameField()
         {
             GameCanvas.Children.Clear();
+            double canvasCenterX = GameCanvas.ActualWidth / 2;
+            double canvasCenterY = GameCanvas.ActualHeight / 2;
+            double scaleFactor = Math.Min(GameCanvas.ActualWidth / 300, GameCanvas.ActualHeight / 500);
+            double adaptiveTileSize = tileSize * scaleFactor;
+            double figureWidth = adaptiveTileSize * (gameField.MaxLayers + 1);
+            double figureHeight = adaptiveTileSize * (gameField.MaxLayers + 1);
+            double offsetX = canvasCenterX - figureWidth / 2;
+            double offsetY = canvasCenterY - figureHeight / 2;
 
             foreach (var tile in gameField.Tiles.OrderBy(t => t.Layer))
             {
                 if (tileSprites == null || tileSprites.Length == 0 || tile.Type >= tileSprites.Length)
                     continue;
 
-                Brush backgroundBrush = tile.IsAvailable
-                    ? Brushes.Beige 
-                    : new SolidColorBrush(Color.FromRgb(150, 200, 255));
-
                 Border border = new Border
                 {
-                    Width = 50,
-                    Height = 50,
-                    Background = backgroundBrush,
+                    Width = adaptiveTileSize,
+                    Height = adaptiveTileSize,
+                    Background = Brushes.Beige,
                     BorderBrush = Brushes.DarkGray,
                     BorderThickness = new Thickness(2),
-                    Margin = new Thickness(tile.Layer * 5, tile.Layer * 5, 0, 0),
-                    CornerRadius = new CornerRadius(8)
+                    CornerRadius = new CornerRadius(8),
+                    Tag = tile
                 };
 
                 Image image = new Image
@@ -71,37 +95,46 @@ namespace MahjongGame
                 };
                 border.Child = image;
 
-                Canvas.SetLeft(border, tile.X * 50);
-                Canvas.SetTop(border, tile.Y * 50);
+                double tileOffsetX = offsetX + tile.X * adaptiveTileSize + tile.Layer * 7 * scaleFactor;
+                double tileOffsetY = offsetY + tile.Y * adaptiveTileSize - tile.Layer * 7 * scaleFactor;
 
-                border.MouseLeftButtonDown += (s, e) =>
-                {
-                    if (tile.IsAvailable && collectionField.AddTile(tile))
-                    {
-                        gameField.RemoveTile(tile);
-                        var removedTiles = collectionField.CheckForTriplets();
-                        if (removedTiles.Any())
-                        {
-                            RenderCollectionField();
-                        }
-                        RenderGameField();
-                        RenderCollectionField();
-                    }
-                };
+                Canvas.SetLeft(border, tileOffsetX);
+                Canvas.SetTop(border, tileOffsetY);
 
                 GameCanvas.Children.Add(border);
+
+                if (!tile.IsAvailable)
+                {
+                    Border overlay = new Border
+                    {
+                        Width = adaptiveTileSize,
+                        Height = adaptiveTileSize,
+                        Background = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0)),
+                        CornerRadius = new CornerRadius(8)
+                    };
+
+                    Canvas.SetLeft(overlay, tileOffsetX);
+                    Canvas.SetTop(overlay, tileOffsetY);
+                    GameCanvas.Children.Add(overlay);
+                }
+
+                border.MouseLeftButtonDown += (s, e) => Tile_Click(tile, border);
             }
         }
+
         private void RenderCollectionField()
         {
+            if (CollectionFieldPanel == null)
+                return;
+
             CollectionFieldPanel.Children.Clear();
 
             for (int i = 0; i < 7; i++)
             {
                 Border border = new Border
                 {
-                    Width = 50,
-                    Height = 50,
+                    Width = 70,
+                    Height = 70,
                     Background = Brushes.LightGray,
                     BorderBrush = Brushes.Black,
                     BorderThickness = new Thickness(2),
@@ -128,54 +161,89 @@ namespace MahjongGame
                 CollectionFieldPanel.Children.Add(border);
             }
 
-            var removedTiles = collectionField.CheckForTriplets();
-            if (removedTiles.Any())
-            {
-                foreach (var tile in removedTiles)
-                {
-                    var borderToRemove = CollectionFieldPanel.Children
-                        .OfType<Border>()
-                        .FirstOrDefault(b => b.Child is Image img && img.Source == tileSprites[tile.Type]);
-
-                    if (borderToRemove != null)
-                    {
-                        AnimateTileRemoval(borderToRemove);
-                    }
-                }
-            }
+            RemoveTriplets();
 
             if (collectionField.IsFull)
             {
                 MessageBox.Show("Вы проиграли! Поле заполнено.", "Проигрыш", MessageBoxButton.OK, MessageBoxImage.Information);
                 StartNewGame();
             }
+
+            CheckForWin();
         }
 
+        private void Tile_Click(Tile tile, Border border)
+        {
+            if (!tile.IsAvailable || collectionField.IsFull)
+                return;
+
+            if (collectionField.AddTile(tile))
+            {
+                gameField.RemoveTile(tile);
+                RenderGameField();
+                RenderCollectionField();
+            }
+        }
+
+        private void RemoveTriplets()
+{
+    var removedTiles = collectionField.CheckForTriplets();
+
+    if (removedTiles.Any())
+    {
+        foreach (var tile in removedTiles)
+        {
+            var borderToRemove = CollectionFieldPanel.Children
+                .OfType<Border>()
+                .FirstOrDefault(b => b.Child is Image img && img.Source == tileSprites[tile.Type]);
+
+            if (borderToRemove != null)
+            {
+                AnimateTileContentRemoval(borderToRemove);
+            }
+        }
+
+        RenderCollectionField();
+    }
+}
+
+        private void AnimateTileContentRemoval(Border border)
+        {
+            var fadeOut = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = TimeSpan.FromSeconds(0.5),
+                FillBehavior = FillBehavior.Stop
+            };
+
+            fadeOut.Completed += (s, e) =>
+            {
+                border.Child = null;
+            };
+
+            if (border.Child is UIElement child)
+            {
+                child.BeginAnimation(UIElement.OpacityProperty, fadeOut);
+            }
+        }
+
+        private void CheckForWin()
+        {
+            if (gameField.Tiles.Count == 0)
+            {
+                MessageBox.Show("Поздравляем! Вы выиграли, все плитки разложены.", "Победа", MessageBoxButton.OK, MessageBoxImage.Information);
+                StartNewGame();
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            RenderGameField();
+        }
         private void StartNewGame(object sender, RoutedEventArgs e)
         {
             StartNewGame();
         }
-
-        private void AnimateTileRemoval(Border tile)
-        {
-            var fadeOutAnimation = new DoubleAnimation
-            {
-                From = 1.0,
-                To = 0.0,
-                Duration = TimeSpan.FromSeconds(2.5),
-                FillBehavior = FillBehavior.Stop
-            };
-
-            fadeOutAnimation.Completed += (s, e) =>
-            {
-                if (tile.Parent is Panel parentPanel)
-                {
-                    parentPanel.Children.Remove(tile);
-                }
-            };
-
-            tile.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation);
-        }
-
     }
 }
